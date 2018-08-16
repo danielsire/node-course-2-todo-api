643aa8d5d01a51e415fdb6b985bf4fe26fb834b1
Daniel Sire <sirehorta@gmail.com>
2018-08-16 20:32:15 -0300

    setting for heroku

--- server/server.js
+++ server/server.js
@@ -17,6 +17,8 @@ var {
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -61,8 +63,8 @@ app.get('/todos/:id', (req, res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {
