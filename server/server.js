3763fa9229e82a99374142acd0dda8f2ad326f77
Daniel Sire <sirehorta@gmail.com>
2018-08-18 14:27:57 -0300

    add PATCH todos id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash')
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const {
     ObjectId
@@ -80,6 +81,40 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     })
+});
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {
+            $set: body
+        }, {
+            new: true
+        })
+        .then((todo) => {
+            if (!todo) {
+                res.status(404).send();
+            }
+
+            res.send({
+                todo
+            });
+        }).catch((e) => {
+            res.status(400).send();
+        })
+
 });
 
 app.listen(port, () => {
